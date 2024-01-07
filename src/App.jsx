b9f73540f3042abc1b919f49195a51254aeefdff
RLAGAMING007 <k.l.kumar006@gmail.com>
2024-01-07 14:59:29 +0530

    landing page navbar added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+const LandingPage = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const HandelClick = () => {
+    setIsActive(!isActive);
+  };
+
+  return (
+    <>
+      <div className="xwrapper">
+        <div className="xheader">
+          <div className="container">
+            <div className="nav-menu">
+              <button
+                className={isActive ? "nav-menu__btn active" : "nav-menu__btn"}
+                onClick={HandelClick}
+              >
+                <MenuIcon
+                  style={{
+                    height: "16px",
+
+                    stroke: isActive ? "#ffdd95" : "white",
+                  }}
+                />
+                <p>Menu</p>
+              </button>
+            </div>
+            <nav className={isActive ? "xheader__nav" : "xheader__nav active"}>
+              <ul className="nav header__menu-list">
+                <CustomLink to="/home">Home</CustomLink>
+                <CustomLink to="/movies">Movies</CustomLink>
+                <CustomLink to="/tv">TV Series</CustomLink>
+                <CustomLink to="/popular">Most Popular</CustomLink>
+                <CustomLink to="/top-ariving">Top Airing</CustomLink>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const CustomLink = ({ to, children, ...props }) => {
+  const resolvedPath = useResolvedPath(to);
+  const isActivePath = useMatch({ path: resolvedPath.pathname, end: true });
+  return (
+    <>
+      <li className="nav-item">
+        <Link className="a" to={to} {...props}>
+          {children}
+        </Link>
+      </li>
+    </>
+  );
+};
+
+export default LandingPage;

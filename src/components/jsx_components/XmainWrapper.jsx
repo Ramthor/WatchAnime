bc696864ab1aaaea619065c3253e4f4ebac7306c
Ramthor <ramkumar978678@gmail.com>
2024-01-21 18:37:57 +0530

    added mediaquary for responsive design..

--- src/components/jsx_components/XmainWrapper.jsx
+++ src/components/jsx_components/XmainWrapper.jsx
@@ -8,12 +8,13 @@ const XmainWrapper = () => {
         <div className="xmain-wrapper__top">
           <div className="mwt-content">
             <div className="mwt-content__div">
-              <div className="cover-img">
-                <img src="assets/onepice.png" alt="" />
-              </div>
               <div className="logo">
                 <img src="/assets/logo-1.png" alt="" />
               </div>
+              <div className="cover-img">
+                <img src="assets/onepice.png" alt="" />
+              </div>
+
               <div className="xseach">
                 <div class="search-content">
                   <form action="/search" autocomplete="off" id="search-form">
